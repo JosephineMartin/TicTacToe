fc7ab30016c877367921877b0ef12c5408a3d516
JosephineMartin <joma1927@colorado.edu>
2020-02-13 16:02:14 -0700

    Person 1 Version of print board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void CreateBoard()
+vector<vector<int> > CreateBoard()
 {
-  int array[3][3];
+  vector<vector<int> > v{ { 0, 0, 0 },
+                          { 0, 0, 0 },
+                          { 0, 0, 0 } };
+  return v;
+}
+
+void DisplayBoard(vector<vector<int> > v)
+{
+  for (int i = 0; i<(int)v.size(); i++)
+  {
+    for (int j = 0; j<(int)v[i].size(); j++)
+    {
+      cout << v[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
 
 int main()
 {
+  vector<vector<int>> v = CreateBoard();
+  DisplayBoard(v);
   return 0;
 }
